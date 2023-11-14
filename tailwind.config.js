c0f83edfff6d84197ae4589354b322a24ed88e73
pedroforadori <foradorif@gmail.com>
2023-11-14 01:11:30 -0300

    build: add icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}", 
+    "./src/components/*.tsx",
+    "./src/screens/*.tsx"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  theme: {
+    colors: {
+      'custom-black': '#121212',
+      'custom-black-secundary': '#363636',
+      'custom-purple': '#8687E7',
+      'custom-white': '#fafafa'
+    }
+  }
 }
 
