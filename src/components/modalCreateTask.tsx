956edd15aa4df63562c93305b617f89b2f0dc230
pedroforadori <foradorif@gmail.com>
2023-11-14 02:37:53 -0300

    feat: add config layout key with modal

--- src/components/modalCreateTask.tsx
+++ src/components/modalCreateTask.tsx
@@ -1,35 +1,42 @@
-import { View, Text, TextInput, Pressable, Image } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { ModalCreateTaskType } from "../screens/modalCreateTask";
 
 // import { Container } from './styles';
 
 export default function ModalCreateTask({ handleClose }: ModalCreateTaskType) {
   return (
-    <View className="bg-custom-fade flex-1 items-center justify-end">
-      <View className="bg-custom-black-secundary w-full py-6 px-4 h-52">
-        <Text className="text-custom-white text-left text-lg">
-          Adicionar Tarefa
-        </Text>
-        <TextInput
-          placeholder="Descrição da tarefa"
-          className="placeholder-custom-white max-h-20"
-          placeholderTextColor="#fafafa"
-          autoFocus
-          multiline
-        />
-        <View className='flex-row justify-around items-center w-full py-10'>
-          <Pressable
-            className=' text-custom-purple'
-            onPress={handleClose}
-          >
-            <Text className='text-custom-purple text-xl'>Cancel</Text>
-          </Pressable>
-          <Pressable className='bg-custom-purple px-2 py-3 w-28 items-center rounded'>
-            <Text className='text-custom-white text-xl'>Criar</Text>
-          </Pressable>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      className='bg-custom-fade flex-1 items-center justify-end'
+    >
+        <View className="bg-custom-black-secundary w-full py-6 px-4 h-52">
+          <Text className="text-custom-white text-left text-lg">
+            Adicionar Tarefa
+          </Text>
+          <TextInput
+            placeholder="Descrição da tarefa"
+            className="placeholder-custom-white max-h-10"
+            placeholderTextColor="#fafafa"
+            autoFocus
+            multiline
+          />
+          <View className="flex-row justify-around items-center w-full py-10">
+            <Pressable className=" text-custom-purple" onPress={handleClose}>
+              <Text className="text-custom-purple text-xl">Cancel</Text>
+            </Pressable>
+            <Pressable className="bg-custom-purple px-2 py-3 w-28 items-center rounded">
+              <Text className="text-custom-white text-xl">Criar</Text>
+            </Pressable>
+          </View>
         </View>
-      </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
